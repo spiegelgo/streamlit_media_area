87302c3492b854758d4d67895633a2dd4f414107
spiegelgo <167832064+spiegelgo@users.noreply.github.com>
2024-05-09 11:13:18 +0900

    텍스트 편집

--- home.py
+++ home.py
@@ -6,18 +6,15 @@ import streamlit as st
 
 def run_home() :
     st.markdown('## ✨컨텐츠에 등장한 그 장면!✨')
-    st.markdown('### 궁금한 그 장소를 찾아드립니다')
+    st.markdown('### 궁금한 그 장소를 찾아드립니다.')
     
     st.markdown('해당 데이터셋은 문화 빅데이터 플랫폼에서 내려받았으며')
-    st.markdown('한국 문화 정보원에서 제공한')
-    st.markdown('KC_MEDIA_VIDO_AREA_DATA_2023.csv를 사용하였습니다')
+    st.markdown("'한국 문화 정보원'에서 제공한")
+    st.markdown('KC_MEDIA_VIDO_AREA_DATA_2023.csv를 사용하였습니다.')
     st.link_button('데이터 제공사이트','https://www.bigdata-culture.kr/bigdata/user/data_market/detail.do?id=462fe230-0334-11ee-a67e-69239d37dfae')
     
     st.markdown('미디어콘텐츠(넷플릭스 등) 영상 촬영지 데이터입니다.')
     st.markdown('인기 촬영장소 위치정보 제공 서비스 활용')
-    st.markdown('전국 인기 촬영지 외국인 방문 및 소비 현황 분석')
-    st.markdown('마지막 기록이 2023.06.05 업데이트라 현재와 다를 수 있음')
-    
-    
+    st.markdown('마지막 기록이 2022.11.25 업데이트라 현재와 다를 수 있음')
     
     st.image('./data/image.png')
